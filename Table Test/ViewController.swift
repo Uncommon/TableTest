bd5bc39f5fb4ddf740a9ed6f5054882fa0760ae0
David Catmull <dtcatmull@verisk.com>
2017-12-15 08:36:41 -0700

    Add a table

--- Table Test/ViewController.swift
+++ Table Test/ViewController.swift
@@ -1,7 +1,8 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+  @IBOutlet weak var tableView: NSTableView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
 
@@ -17,3 +18,42 @@ class ViewController: NSViewController {
 
 }
 
+extension ViewController: NSTableViewDelegate
+{
+  struct ColumnID
+  {
+    static let file = NSUserInterfaceItemIdentifier("File")
+    static let other = NSUserInterfaceItemIdentifier("Other")
+  }
+  
+  func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView?
+  {
+    guard let identifier = tableColumn?.identifier,
+          let cell = tableView.makeView(withIdentifier: identifier, owner: self) as? NSTableCellView
+    else { return nil }
+    
+    switch identifier {
+      case ColumnID.file:
+        cell.textField?.stringValue = "Name"
+        cell.imageView?.image = NSImage(named: .colorPanel)
+      case ColumnID.other:
+        cell.textField?.stringValue = "Stuff"
+      default:
+        return nil
+    }
+    return cell
+  }
+}
+
+extension ViewController: NSTableViewDataSource
+{
+  func numberOfRows(in tableView: NSTableView) -> Int
+  {
+    return 1
+  }
+  
+  func tableView(_ tableView: NSTableView, pasteboardWriterForRow row: Int) -> NSPasteboardWriting?
+  {
+    return NSPasteboardItem(pasteboardPropertyList: "some text", ofType: .string)
+  }
+}
